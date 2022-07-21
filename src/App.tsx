2b8bfc36d6baa0dc8318e6abe9fcb66e4b4e3244
Steven Jimenez <steven.jimenez@washpost.com>
2022-07-21 11:05:30 -0400

    Toy example forwards events from iframe to parent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const iframe = useRef<HTMLIFrameElement>(null)
+  const [events, setEvents] = useState<React.KeyboardEvent[]>([])
+
+  useEffect(() => {
+    const current = iframe.current
+    const document = current?.contentDocument || current?.contentWindow?.document
+    if (current && document) {
+      const handle = (event: KeyboardEvent) => {
+        const evt = new KeyboardEvent('keydown', {
+          key: event.key,
+          shiftKey: event.shiftKey,
+          ctrlKey: event.ctrlKey,
+          altKey: event.altKey,
+          metaKey: event.metaKey
+        });
+        current?.dispatchEvent(evt)
+      }
+      document.addEventListener('keydown', handle)
+      return () => {
+        document.removeEventListener('keydown', handle)
+      }
+    }
+  })
+
+  return (<div style={{display: 'flex'}}>
+      <section style={{flex: 1}}>
+        <header>Events</header>
+        <ul>{events.map((evt, i) => (<li
+            key={`${events.length - i}`}>{`${evt.metaKey ? 'Meta+' : ''}${evt.altKey ? 'Alt+' : ''}${evt.ctrlKey ? 'Ctrl+' : ''}${evt.shiftKey ? 'Shift+' : ''}${evt.key.toUpperCase()}`}</li>))}</ul>
+      </section>
+      <section style={{flex: 1}}>
+        <header>Frame</header>
+        <iframe title="target" ref={iframe} onKeyDownCapture={(evt) => {
+          switch (evt.key.toUpperCase()) {
+            case 'META':
+            case 'SHIFT':
+            case 'CONTROL':
+            case 'ALT':
+              return;
+          }
+          setEvents([evt, ...events])
+        }}/>
+      </section>
+    </div>);
 }
 
 export default App;
